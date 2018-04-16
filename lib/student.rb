79a317288f6cd4007309d20ffc4a26f51ac4a90d
Stephen Harrington <stephenharrington04@yahoo.com>
2018-04-16 20:28:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,35 +2,15 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    fng = Student.new
-    fng.id = row[0]
-    fng.name = row[1]
-    fng.grade = row[2]
-    fng
+
   end
 
   def self.all
-    sql = <<-SQL
-      SELECT *
-      FROM students
-    SQL
 
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
   end
 
   def self.find_by_name(name)
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE students.name = ?
-      LIMIT 1
-    SQL
 
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end.first
   end
 
   def save
@@ -59,66 +39,6 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def self.count_all_students_in_grade_9
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE students.grade = 9
-    SQL
-
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
-  end
-
-  def self.students_below_12th_grade
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE students.grade <= ?
-    SQL
-
-    DB[:conn].execute(sql, 11).map do |row|
-      self.new_from_db(row)
-    end
-  end
-
-  def self.first_X_students_in_grade_10(number)
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE students.grade = 10
-      LIMIT ?
-    SQL
-
-    DB[:conn].execute(sql, number).map do |row|
-      self.new_from_db(row)
-    end
-  end
-
-  def self.first_student_in_grade_10
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE students.grade = 10
-      LIMIT 1
-    SQL
-
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end.first
-  end
-
-  def self.all_students_in_grade_X(grade)
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE students.grade = ?
-    SQL
-
-    DB[:conn].execute(sql, grade).map do |row|
-      self.new_from_db(row)
-    end
-  end
+  
 
 end
